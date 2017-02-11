79fad31e5efbf26a2f29b02c531611076e7e1cd6
sherif20766 <sherif20766@hotmail.com>
2017-02-11 16:38:41 +0000

    included jade templating, set up for mySQL

--- main.js
+++ main.js
@@ -1,19 +1,46 @@
 var express = require("express");
-var app = express();
+var mysql	= require('mysql');
+var app 	= express();
 
-app.use(express.static("public"));
+//Application Settings
+app.use(express.static("public")); //setting up "static folder"
+app.set("view engine", "jade"); //jade template engine
+
+//Configuration
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'me',
+  password : 'secret',
+  database : 'my_db'
+});
+ 
 
 app.get("/", function(req, res){
-	res.sendFile(__dirname +"/index.html");
+
+	//set up connection with database
+	
+	connection.connect();
+ 
+	connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+	  if (error) throw error;
+	  console.log('The solution is: ', results[0].solution);
+	});
+	 
+	connection.end();
+
+	/*-------------------------------*/
+
+
+	//res.sendFile(__dirname +"/index.html");
+	res.render("index", {title: "My Title", message: "Hello World"}); //render express "view"
 });
 
 app.get("/goodbye", function(req, res){
 	res.send("goodbye");
 });
 
-var server  = app.listen(8080, function(){
-	var host = server.address().address;
-	var port = server.address().port;
+var server = app.listen(8080, function(){
 
-	console.log("your app is running at http://%s:%s", host, port);
+
+	console.log("your app is running at http://localhost:8080");
 });
